2e8dad82f490316c025daa5b558554bf08e8f9f8
Lyle Johnson <lyle@lylejohnson.name>
2011-01-07 15:31:55 -0600

    Added some customizations for Org mode

--- lyle.el
+++ lyle.el
@@ -10,3 +10,10 @@
   (shell-command
   (format "%s -f %s/TAGS -e -R --langmap=Lisp:+.clj %s" path-to-ctags dir-name (directory-file-name dir-name)))
 )
+
+;; Org mode customizations per the tutorial
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-modee))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
