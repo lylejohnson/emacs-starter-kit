bfd699d55d590a9f124c0b169597d54cf329383e
Lyle Johnson <lyle@lylejohnson.name>
2011-01-07 15:30:59 -0600

    Added a create-tags-file function.

--- lyle.el
+++ lyle.el
@@ -1,3 +1,12 @@
 ;; Put user-specific configuration stuff in here.
 (add-to-list 'package-archives
              '("technomancy" . "http://repo.technomancy.us/emacs/") t)
+
+; 
+(setq path-to-ctags "/usr/local/bin/ctags") ;; <- your ctags path here
+(defun create-tags-file (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (shell-command
+  (format "%s -f %s/TAGS -e -R --langmap=Lisp:+.clj %s" path-to-ctags dir-name (directory-file-name dir-name)))
+)
