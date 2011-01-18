980f03f3f3a64fad77716e1a19df413e87e194b0
Lyle Johnson <lyle@lylejohnson.name>
2011-01-18 11:13:30 -0600

    Added startup script for nxhtml mode

--- lyle.el
+++ lyle.el
@@ -41,3 +41,18 @@
 ;; CoffeeScript
 (add-to-list 'load-path "~/.emacs.d/lyle/coffee-mode")
 (require 'coffee-mode)
+
+;; nXHTML
+(load "~/.emacs.d/lyle/nxhtml/autostart.el")
+(setq
+ nxhtml-global-minor-mode t
+ mumamo-chunk-coloring 'submode-colored
+ nxhtml-skip-welcome t
+ indent-region-mode t
+ rng-nxml-auto-validate-flag nil
+ nxml-degraded t)
+(add-to-list 'auto-mode-alist '("\\.html\\.erb$" .  eruby-nxhtml-mumamo-mode))
+(add-to-list 'auto-mode-alist '("\\.rhtml$" .  eruby-nxhtml-mumamo-mode))
+
+;; Rinari
+(setq rinari-tags-file-name "TAGS")
